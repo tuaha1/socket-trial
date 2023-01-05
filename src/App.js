13def5c01b0463761026f60f8905752121f5d7d4
tuaha1 <tuahakst@gmail.com>
2023-01-05 15:05:28 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import io from "socket.io-client"
+
+const socket = io("https://socket-trial-server.onrender.com")
 
 function App() {
+
+  const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+
+  useEffect(() => {
+    socket.on("message", (data) => {
+      console.log(data);
+      setMessageList(prevData => [...prevData, data]);
+    })
+  }, [])
+
+  const handleClick = () => {
+    socket.emit("send message", message);
+    setMessage('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>Send message</h1>
+        <input value={message} onChange={(e) => { setMessage(e.target.value); }} placeholder="enter message" />
+        <button onClick={handleClick}>submit</button>
+        {messageList.map((element, index) => {
+          return <li key={index}>{element}</li>
+        })}
+      </center>
     </div>
   );
 }
